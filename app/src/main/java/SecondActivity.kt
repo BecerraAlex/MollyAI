8541489584593b8f5006c5c64a7bd57975261017
BecerraAlex <lx.bcrr@gmail.com>
2024-09-16 05:04:35 -0700

    V1.0

---
+++ app/src/main/java/SecondActivity.kt
@@ -0,0 +1,12 @@
+//Fitness Page
+package com.example.mollyai
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)  // This is referring to the activity_second.xml layout file
+    }
+}
